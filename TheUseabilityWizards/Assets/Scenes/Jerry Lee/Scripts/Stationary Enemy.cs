2f776a777a86c6d44667f5ef41a2fce153c50be9
jerrylee226 <jerrylee226.jl@gmail.com>
2024-06-11 22:45:37 -0700

    Added Stationary Enemy script
    enemy script for a stationary ranged enemy, does not move but aim and attack player. Needs testing with an enemy

---
+++ TheUseabilityWizards/Assets/Scenes/Jerry Lee/Scripts/Stationary Enemy.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StationaryEnemy : MonoBehaviour
+{
+
+    [SerializeField] Renderer Model;
+    [SerializeField] NavMeshAgent enemyAgent;
+    [SerializeField] Animator anim;
+    [SerializeField] Transform shootPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int animTransitionSpeed;
+    [SerializeField] int playerTargetSpeed;
+
+    bool isShooting;
+    bool playerInRange;
+
+    [SerializeField] float shootRate;
+    [SerializeField] GameObject arrow;
+
+    Vector3 playerDirect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //update game goal code here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDirect = gameManager.instance.player.transform.position - transform.position;
+
+        if (playerInRange)
+        {
+
+            faceTarget();
+
+            if (!isShooting)
+            {
+                StartCoroutine(shoot());
+            }
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rotate = Quaternion.LookRotation(playerDirect);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotate, Time.deltaTime * playerTargetSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        anim.SetTrigger("Shoot");
+
+        Instantiate(arrow, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    IEnumerator flashDamage()
+    {
+        Model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        Model.material.color = Color.white;
+    }
+
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashDamage());
+        //stationary enemy should face the player's last known location upon being shot
+        faceTarget();
+
+        if(HP <= 0)
+        {
+            //game goal update here
+
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
