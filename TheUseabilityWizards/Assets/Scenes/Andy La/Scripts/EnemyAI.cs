ebc2ca549d053a129ea9015d0680a4f4b8bdb9cc
Andy La <ALa@student.fullsail.edu>
2024-06-12 13:18:29 -0700

    Testing
    Testing

---
+++ TheUseabilityWizards/Assets/Scenes/Andy La/Scripts/EnemyAI.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] int HP;
+
+    [SerializeField] Animator animator;
+    [SerializeField] int animatorTranSpeed;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] float attackRate;
+
+    bool isAttacking;
+    bool isPlayerInRange;
+
+    Vector3 playerDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameManager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDir = gameManager.instance.player.transform.position - transform.position;
+
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), agentSpeed, Time.deltaTime));
+
+        if (isPlayerInRange)
+        {
+            agent.SetDestination(gameManager.instance.player.transform.position);
+
+            if (agent.remainingDistance < agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        isPlayerInRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isPlayerInRange = false;
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashDamage());
+
+        if (HP <= 0)
+        {
+            //gameManager.instance.updateGameGoals(-1);
+            Destroy(gameObject);
+        }
+    }
+
+    //IEnumerator attack(int amount)
+    //{
+    //    isAttacking = true;
+    //    animator.SetTrigger("Attack");
+
+    //    yield return new WaitForSeconds(attackRate);
+    //    isAttacking = false;
+    //}
+
+    IEnumerator flashDamage()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+}
