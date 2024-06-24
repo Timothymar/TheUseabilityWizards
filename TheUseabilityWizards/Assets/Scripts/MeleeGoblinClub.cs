404c050dc2471a69b9a00678cd6a516525eb7166
Andy La <ALa@student.fullsail.edu>
2024-06-24 11:53:33 -0700

    Goblin Club Updated

--- TheUseabilityWizards/Assets/Scripts/MeleeGoblinClub.cs
+++ TheUseabilityWizards/Assets/Scripts/MeleeGoblinClub.cs
@@ -7,13 +7,14 @@ public class MeleeGoblinClub : MonoBehaviour
     [SerializeField] Rigidbody rb;
 
     [SerializeField] int damage;
+    [SerializeField] Collider weaponCol;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
- 
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
@@ -33,5 +34,15 @@ public class MeleeGoblinClub : MonoBehaviour
             }
         }
         //Destroy(gameObject);
+    }
+
+    public void weaponColOn()
+    {
+        weaponCol.enabled = true;
+    }
+    
+    public void weaponColOff() 
+    {
+        weaponCol.enabled = false;
     }
 }
