8e64302803008821adc3038a8b6f3e0e4622b78b
Andy La <ALa@student.fullsail.edu>
2024-06-26 18:48:59 -0700

    Mage can roam around

--- TheUseabilityWizards/Assets/Scripts/MageEnemyAI.cs
+++ TheUseabilityWizards/Assets/Scripts/MageEnemyAI.cs
@@ -8,19 +8,30 @@ public class MageEnemyAI : MonoBehaviour
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Animator anima;
+    [SerializeField] Transform headPos;
     [SerializeField] Transform CastPos;
 
+    [SerializeField] int HP;
     [SerializeField] int animTranSpeed;
     [SerializeField] int faceTargetSpeed;
-    [SerializeField] int HP;
-    [SerializeField] float castRate;
+    [SerializeField] int visionCone;
 
+    [SerializeField] float castRate;
+    [SerializeField] int castAngle;
     [SerializeField] GameObject fireBall;
+
+    [SerializeField] int roamDist;
+    [SerializeField] int roamTimer;
 
     bool isCasting;
     bool playerInRange;
+    bool destChosen;
 
     Vector3 playerDir;
+    Vector3 startingPos;
+
+    float angleToPlayer;
+    float stoppingDistOrig;
 
     // Start is called before the first frame update
     void Start()
@@ -31,25 +42,52 @@ public class MageEnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerDir = gameManager.instance.player.transform.position - transform.position;
-
         float agentSpeed = agent.velocity.normalized.magnitude;
         anima.SetFloat("Speed", Mathf.Lerp(anima.GetFloat("Speed"), agentSpeed, Time.deltaTime * animTranSpeed));
 
-        if (playerInRange)
+        if (playerInRange && !canSeePlayer())
         {
-            agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(roam());
+        }
+        else if (!playerInRange)
+        {
+            StartCoroutine(roam());
+        }
+    }
 
-            if (agent.remainingDistance < agent.stoppingDistance)
-            {
-                faceTarget();
-            }
+    bool canSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, playerDir.y + 1, playerDir.z), transform.forward);
+        //Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, new Vector3(playerDir.x, playerDir.y + 1, playerDir.z));
 
-            if (!isCasting)
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            Debug.Log(hit.collider.name);
+
+            // Hey I can see player
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= visionCone)
             {
-                StartCoroutine(cast());
+                agent.stoppingDistance = stoppingDistOrig;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                if (!isCasting && angleToPlayer <= castAngle)
+                {
+                    StartCoroutine(cast());
+                }
+                return true;
             }
         }
+
+        agent.stoppingDistance = 0;
+        return false;
     }
 
     void faceTarget()
@@ -60,12 +98,18 @@ public class MageEnemyAI : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        playerInRange = true;
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
-        playerInRange = false;
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
     }
 
     IEnumerator cast()
@@ -101,5 +145,27 @@ public class MageEnemyAI : MonoBehaviour
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = Color.white;
+    }
+
+    IEnumerator roam()
+    {
+        if (!destChosen && agent.remainingDistance < 0.05f)
+        {
+            destChosen = true;
+            yield return new WaitForSeconds(roamTimer);
+
+            agent.stoppingDistance = 0;
+
+            // Keep his roam area small
+            Vector3 ranPos = Random.insideUnitSphere * roamDist;
+            ranPos += startingPos;
+
+            // Keeps on the NavMesh
+            NavMeshHit hit;
+            NavMesh.SamplePosition(ranPos, out hit, roamDist, 1);
+            agent.SetDestination(hit.position);
+
+            destChosen = false;
+        }
     }
 }
