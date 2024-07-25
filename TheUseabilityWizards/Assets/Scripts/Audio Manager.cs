bfb4bab33dd7c2a7aba7ae5ab6891ad28d0dd941
Andy La <ALa@student.fullsail.edu>
2024-07-24 21:48:58 -0700

    Background Music Added 2 more

--- TheUseabilityWizards/Assets/Scripts/Audio Manager.cs
+++ TheUseabilityWizards/Assets/Scripts/Audio Manager.cs
@@ -1,18 +1,72 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class AudioManager : MonoBehaviour
+//{
+//    [Header("-----Audio Source-----")]
+//    [SerializeField] AudioSource bgSource;
+
+//    [Header("-----Audio Clip-----")]
+//    public AudioClip background;
+
+//    private void Start()
+//    {
+//        bgSource.clip = background;
+//        bgSource.Play();
+//    }
+//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("-----Audio Source-----")]
-    [SerializeField] AudioSource bgSource;
+    [Header("----- Audio Source -----")]
+    [SerializeField] private AudioSource bgSource;
 
-    [Header("-----Audio Clip-----")]
-    public AudioClip background;
+    [Header("----- Audio Clips -----")]
+    [SerializeField] private List<AudioClip> backgroundTracks;
+
+    private int currentTrackIndex = 0;
 
     private void Start()
     {
-        bgSource.clip = background;
-        bgSource.Play();
+        if (backgroundTracks.Count > 0)
+        {
+            PlayTrack(currentTrackIndex);
+        }
+    }
+
+    private void Update()
+    {
+        if (!bgSource.isPlaying)
+        {
+            PlayNextTrack();
+        }
+    }
+
+    private void PlayNextTrack()
+    {
+        currentTrackIndex = (currentTrackIndex + 1) % backgroundTracks.Count;
+        PlayTrack(currentTrackIndex);
+    }
+
+    private void PlayTrack(int trackIndex)
+    {
+        if (trackIndex >= 0 && trackIndex < backgroundTracks.Count)
+        {
+            bgSource.clip = backgroundTracks[trackIndex];
+            bgSource.Play();
+        }
+    }
+
+    public void PlaySpecificTrack(int trackIndex)
+    {
+        if (trackIndex >= 0 && trackIndex < backgroundTracks.Count)
+        {
+            currentTrackIndex = trackIndex;
+            PlayTrack(currentTrackIndex);
+        }
     }
 }
