bf05d07492c6c95fcdeb7cc4ba9d6e42b85c4b55
Timothy Martin <TJMARTIN7@STUDENT.FULLSAIL.EDU>
2024-07-27 03:24:35 -0700

    Made an FPS limiter.

---
+++ TheUseabilityWizards/Assets/Scripts/FPSLimiter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+    public int targetFPS = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
